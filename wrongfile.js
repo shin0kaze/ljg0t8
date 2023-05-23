901281d8fb31e307f16a9cb91a14d3ef0d0caa96
usr <kirill.moskva@mail.ru>
2023-05-23 20:40:57 +0600

    message

---
+++ wrongfile.js
@@ -0,0 +1,31 @@
+// let person =                     {
+//   name: "Yoda",
+//                 designation: 'Jedi Master '
+//                 };
+
+//               function trainJedi (jediWarrion) {
+// if (jediWarrion.name === 'Yoda') {
+//   console.log('No need! already trained');
+// }
+// console.log(`Training ${jediWarrion.name} complete`)
+//               }
+
+// trainJedi(person)
+//               trainJedi({ name: 'Adeel',
+//               designation: 'padawan'
+//   });
+
+let person    = {
+  name: 'Yoda',
+  designation: 'Jedi Master ',
+}
+
+function trainJedi(jediWarrion) {
+  if (jediWarrion.name === 'Yoda') {
+    console.log('No need! already trained')
+  }
+  console.log(`Training ${jediWarrion.name} complete`)
+}
+
+trainJedi(person);
+trainJedi({ name: 'Adeel', designation: 'padawan' });
