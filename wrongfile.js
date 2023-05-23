7a88c82e7e7b6ff664d6789f602e7d144cb88cc2
usr <kirill.moskva@mail.ru>
2023-05-23 20:46:10 +0600

    test husky

--- wrongfile.js
+++ wrongfile.js
@@ -15,14 +15,14 @@
 //               designation: 'padawan'
 //   });
 
-let person    = {
+let person       = {
   name: 'Yoda',
   designation: 'Jedi Master ',
 }
 
 function trainJedi(jediWarrion) {
   if (jediWarrion.name === 'Yoda') {
-    console.log('No need! already trained')
+    console.log('No need! already trained');
   }
   console.log(`Training ${jediWarrion.name} complete`)
 }
